b7ccebfad5c782481dd25d988271f1378762b6fa
melissa <melmihomel@gmail.com>
2024-04-10 13:59:53 -0300

    feat: add events and images

---
+++ src/constants/eventLogos.js
@@ -0,0 +1,19 @@
+import img_accident from '../assets/event-accident.png';
+import img_crowd from '../assets/event-crowd.png';
+import img_flood from '../assets/event-flood.png';
+import img_gun from '../assets/event-gun.png';
+import img_kidnapping from '../assets/event-kidnapping.png';
+import img_forgotten_baby from '../assets/images/forgotten-baby.png';
+import img_license_plate from '../assets/images/licence_plate.png'
+
+const EventLogos = {
+    "VEHICLE_ACCIDENT": img_accident,
+    "CROWDING": img_crowd,
+    "FLOOD": img_flood,
+    "FIREARM": img_gun,
+    "KIDNAPPING": img_kidnapping,
+    "FORGOTTEN_BABY": img_forgotten_baby,
+    "LICENSE_PLATE": img_license_plate,
+}
+
+export default EventLogos;
