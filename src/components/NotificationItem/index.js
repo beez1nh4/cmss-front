95c71f97d93ba9e871ba4dc52ab05b89299ac485
melissa <melmihomel@gmail.com>
2024-05-13 12:49:04 -0300

    feat: add license plate value

--- src/components/NotificationItem/index.js
+++ src/components/NotificationItem/index.js
@@ -8,11 +8,17 @@ import { EventLogos } from "../../constants/eventLogos";
 export default function NotificationItem({notification}) {
     const [sentinel, setSentinel] = useState({sentinel_id: 0, didDocument: "did", vehicle: "NA"});
     const [logo, setLogo] = useState()
+    const [plate, setPlate] = useState("-")
 
     useEffect(() => {
         if (sentinels_labels.find((sentinel) => sentinel.didDocument === notification.remote.didDocument.id)){
             setSentinel(sentinels_labels.find((sentinel) => sentinel.didDocument === notification.remote.didDocument.id))
             setLogo(EventLogos[notification.notification_object.events[0].type])
+
+            if(notification.notification_object.events[0].triggered_sensors[0].value){
+                setPlate(notification.notification_object.events[0].triggered_sensors[0].value)
+            }
+
         }
         
     }, [sentinel, notification.remote.didDocument.id, notification.notification_object.events])
@@ -21,10 +27,13 @@ export default function NotificationItem({notification}) {
         const sentinel = sentinels_labels.find((sentinel) => sentinel.didDocument === notification.remote.didDocument.id);
         swal({
             
-            title: `Sentinel ${sentinel.sentinel_id}`,
+            title: `Sentinel ${sentinel.sentinel_id} - Vehicle ${sentinel.vehicle}`,
             text: `Type: ${notification.notification_object.events[0].type}
+
+            Value: ${plate}
+
             Confidence: ${notification.notification_object.events[0].accuracy}`,
-            content:`<img src=${EventLogos.crowding} alt=${EventLogos.crowding} />`,
+            content:`<img src=${EventLogos.crowding} alt="" />`,
           });
     }
 
